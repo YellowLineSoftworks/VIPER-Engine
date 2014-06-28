d79ed501e4471c077440b0b070a9f61c4ca669c6
Jack Hamilton <jackham800@gmail.com>
2014-06-28 13:49:53 -0400

    Patch 3
    Added a function to change the available images in a GameObject.

--- src/resources/GameObject.java
+++ src/resources/GameObject.java
@@ -126,9 +126,43 @@ public class GameObject {
      * Changes the currently displayed sprite.
      * @param id The ID of the sprite to change to.
      */
-    public void changeSprite(int id) {
+    public void changeDisplayedSprite(int id) {
         currentSprite = sprites[id];
         draw();
+    }
+    
+    /**
+     * Changes the available images for the GameObject.
+     * @param images An array of the images to change to.
+     */
+    public void changeSprites(Image[] images) {
+        Sprite[] TempSpriteArray = new Sprite[images.length];
+        for (int c = 0; c < images.length; c++) {
+            TempSpriteArray[c] = new Sprite(images[c], sprites[0].x1, sprites[0].y1, sprites[0].x1 + images[c].getWidth(null), 
+                    sprites[0].y1 + images[c].getHeight(null), 0, 0, images[c].getWidth(null), images[c].getWidth(null));
+        }
+    }
+    
+    /**
+     * Changes the available images for the GameObject.
+     * @param x The x-coordinate of the top-left corner of the object.
+     * @param y The y-coordinate of the top-left corner of the object.
+     * @param endx The x-coordinate of the bottom-right corner of the object.
+     * @param endy The y-coordinate of the bottom-right corner of the object.
+     * @param srcx1 The x-coordinate of the top-left corner of the source image.
+     * @param srcy1 The y-coordinate of the top-left corner of the source image.
+     * @param srcx2 The x-coordinate of the bottom-right corner of the source image.
+     * @param srcy2 The y-coordinate of the bottom-right corner of the source image.
+     * @param images The images that the object can display.
+     */
+    public void changeSprites(int x, int y, int endx, int endy, int srcx1, int srcy1, int srcx2, int srcy2, Image[] images) {
+        this.location = new Point(x,y);
+        this.sprites = new Sprite[images.length];
+        for (int c = 0; c < images.length; c++) {
+            sprites[c] = new Sprite(images[c], x, y, endx, endy, srcx1, srcy1, srcx2, srcy2);
+        }
+        currentSprite = sprites[0];
+        objects.add(this);
     }
     
     /**
