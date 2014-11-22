d6946373ce7d5b21248e53f548a1d0859dd9961a
Jack Hamilton <jackham800@gmail.com>
2014-11-22 10:59:47 -0500

    Prime Patch 1
    Added some cleanup code to the Prime class.

--- src/test.java
+++ src/test.java
@@ -1,4 +1,8 @@
 
+import crypto.Prime;
+import java.math.BigInteger;
+
+
 
 /**
  * An internal class for testing VIPER methods.
@@ -6,11 +10,18 @@
  * @version 1.4 Alpha 
  */
 public class test {
-
+        
     /**
      * @param args The command line arguments
      */
     public static void main(String[] args) {
+        
+        int[] testVectors = Prime.eratosthenes(2000);
+        for (int i: testVectors) {
+            if (i > 25) {
+                System.out.println(Prime.strongLucas(new BigInteger(""+i), false));
+            }
+        }
         
     }
     
